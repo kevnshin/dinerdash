26c3a78e697243badd40b02de8b9e7780772db73
Kevin Shin <kevnshin@gmail.com>
2014-10-11 16:53:05 -1000

    Utility _.now done

---
+++ dinerdash.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+  //return number of milliseconds since Jan 1, 1970 midnight
+  now: function () {
+
+    return new Date().getTime();
+
+  },
+
+  
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
---
+++ runner.js
@@ -0,0 +1,5 @@
+// new file named runner.js
+var _ = require('./dinerdash'); // your "whatever" file name (no .js is needed)
+ 
+var result = _.now();
+..console.log(result);
