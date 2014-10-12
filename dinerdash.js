593d295c3fe7881e107e07457ff429359798958c
Kevin Shin <kevnshin@gmail.com>
2014-10-11 19:33:32 -1000

    Utility _.parseInt function done

--- dinerdash.js
+++ dinerdash.js
@@ -65,14 +65,88 @@ module.exports = {
 
   //skipped mixins
 
+  //skipped noConflict
+
   //no operation function --> do nothing
   noop: function() {
 
 
   },
 
+//currently only works for base 10 and 2
+
+  parseInt: function (string, radix) {
+
+    var number_base;
+
+    //if user inputs a radix number
+    if(typeof radix === "number"){
+      number_base = radix;
+    } else {//user didn't input radix
+      number_base = 10;
+    }
+
+    switch (number_base) {
+
+      case 10:
+        return Number(string);
+        break;
+
+      case 2:
+        var numberbase_10 = Number(string);
+        var backwards = '';
 
 
+
+        while(numberbase_10>=1){
+
+          //if dividing number by 2 has remainder 1
+          if(numberbase_10%2 === 1){
+            backwards += "1";
+            numberbase_10 -= 1;
+            numberbase_10 /= 2;
+
+
+          } else { //dividing number by 2 is 0
+            backwards += "0";
+            numberbase_10 /= 2;
+          }
+
+          
+
+        }
+
+        var number_base2 = '';
+        for(var i = backwards.length - 1; i>=0; i--){
+          number_base2 += backwards[i];
+        }
+
+
+        return Number(number_base2);
+        break;
+
+      default:
+        return Number(string);
+
+
+    } 
+
+
+  },
+
+
+  property: function(key) {
+
+
+
+
+
+
+
+
+
+
+  },
 
 
 
