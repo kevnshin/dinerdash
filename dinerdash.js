b654ee3142f79c84cd7009cc93f90d28dc075b3f
Kevin Shin <kevnshin@gmail.com>
2014-10-11 17:06:10 -1000

    Utility _. constant function done

--- dinerdash.js
+++ dinerdash.js
@@ -1,13 +1,23 @@
 module.exports = {
 
-  //return number of milliseconds since Jan 1, 1970 midnight
-  now: function () {
+  
 
+  //Utilities
+
+  now: function () {
+    //return number of milliseconds since Jan 1, 1970 midnight
     return new Date().getTime();
 
   },
 
-  
+  constant: function (value) {
+
+    //return the value(s) as a function
+    return function () {
+        return value;
+      };
+
+  },
 
 
    
